103d830666cb5ae1aabe6bfad78c2ad30d6e4272
alex <alexsemen666653@gmail.com>
2023-10-28 20:00:55 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/hw2_10/SkyproSpring15Application.java
@@ -0,0 +1,14 @@
+package ru.skypro.hw2_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpring15Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproSpring15Application.class, args);
+	}
+
+}
